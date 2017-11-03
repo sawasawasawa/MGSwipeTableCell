400cd3af9173742abb044998b758388845773827
Mateusz Sawka <m.e.sawka@gmail.com>
2017-11-03 10:40:06 +0100

    addLintEvents

---
+++ new.swift
@@ -0,0 +1,45 @@
+//
+//  ViewController.swift
+//  TestsTest1
+//
+//  Created by Maciej Koziel on 19/09/2017.
+//  Copyright © 2017 Maciej Koziel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    func someTestedFunction() {
+        print("I'm tested")
+    }
+
+    func someTestedFunctionNext() {
+        // Tralalalalal alalalal al lalalla allaal al allalal al al alalaalla lalallal lalalallalalla l l Tralalalalal alalalal al lalalla allaal al allalal al al alalaalla lalallal lalalallalalla l l
+        print("Tralalalalalzzff alalalal al lalalla allaal al allalal al al alalaalla lalallal lalalallalalla l l Tralalalalal alalalal al lalalla allaal al allalal al al alalaalla lalallal lalalallalalla l l")
+        print("I'm tested")
+        print("I'm tested")
+        print("I'm tested")
+        print("I'm tested")
+        print("I'm tested")
+        print("I'm tested")
+        print("I'm tested")
+        print("I'm tested")
+        print("I'm tested")
+        print("I'm tested")
+        print("I'm tested")
+        print("I'm tested")
+        print("I'm tested")
+        print("I'm tested")
+        print("I'm tested")
+        print("I'm tested")
+        print("I'm tested")
+        print("I'm tested")
+        print("I'm tested")
+    }
+
+    func someUntestedFunction() {
+        print("I'm untested")
+    }
+
+}
